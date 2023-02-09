a9aae4adb91b064adbc2db3cc5040aeea5960994
Edygar Franquiz <edyfranquiz@gmail.com>
2023-02-08 22:20:32 -0300

    se modificaron los estilos de la web version movil

--- js/carrito.js
+++ js/carrito.js
@@ -57,12 +57,6 @@
             eliminarProducto(product.id);
         })
 
-        // let eliminar = document.createElement("span");
-        // eliminar.innerText = "❌";
-        // eliminar.className = "delete-product";
-        // carritoContent.append(eliminar);
-
-        // eliminar.addEventListener("click", eliminarProducto);
     });
 
     const total = carrito.reduce((acc , el,) => acc += el.precio * el.cantidad,  0);
