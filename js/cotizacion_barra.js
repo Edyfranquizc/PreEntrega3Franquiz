4a994a5d81aed34ce7893ea6e29019f229408eb5
Edygar Franquiz <edyfranquiz@gmail.com>
2023-02-22 17:53:17 -0300

    se hicieron cambios en el archivo cotizacion_barra.js

--- js/cotizacion_barra.js
+++ js/cotizacion_barra.js
@@ -2,18 +2,18 @@ let barraDeCotizacion =document.querySelector("#barraDeCotizacion");
 
 const fetchCotizacion_barra = async () => {
     try {
-        const response = await fetch(" https://api.getgeoapi.com/v2/currency/convert?api_key=7440e33dd31b89ad749dd9bbdc1e79e5877d00a0");
+        const response = await fetch("https://api.bluelytics.com.ar/v2/evolution.json");
     const data = await response.json();
     console.log(data);
 
     const infoCotizacion = document.createElement("div");
     infoCotizacion.innerHTML = `
-    <h4>${data.base_currency_code}</h4>
-    <h4>${data.base_currency_name}</h4>
-    <h4>${data.base_rates}</h4>
-    <h4>${data.update_date}</h4>
-    
+    <h4>${data.date}</h4>
+    <h4>${data.source}</h4>
+    <h4>${data.value_sell}</h4>
+    <h4>${data.value_buy}</h4>
     `;
+
     barraDeCotizacion.append(infoCotizacion);
     } catch (error){
         console.log(error);
@@ -26,6 +26,15 @@ fetchCotizacion_barra();
 
 
 
+
+
+
+
+
+
+
+/* <h4>${data.rates.AED.rate_for_amount}</h4>
+Esa es la manera de acceder, de rate podés traer los otros 3 valores adicionales como AED (o el que elijas) AED.rate o AED.currency_name */
 
 
 
