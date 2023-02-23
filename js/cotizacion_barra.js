6d42c0c067e4731d506a92b588f113b59d63a2f6
Edygar Franquiz <edyfranquiz@gmail.com>
2023-02-22 21:04:10 -0300

    se agregaron cambios en el archivo  cotizacion_barra.js

--- js/cotizacion_barra.js
+++ js/cotizacion_barra.js
@@ -1,27 +1,66 @@
-let barraDeCotizacion =document.querySelector("#barraDeCotizacion");
+let barraDeCotizacion =document.getElementById("barraDeCotizacion");
 
-const fetchCotizacion_barra = async () => {
-    try {
-        const response = await fetch("https://api.bluelytics.com.ar/v2/evolution.json");
-    const data = await response.json();
-    console.log(data);
+fetch("https://api.bluelytics.com.ar/v2/evolution.json")
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data);
+        for (let infoCotizacion of data){
+            console.log(infoCotizacion);
+            let nuavaInfo = document.createElement("div")
+            nuavaInfo.innerHtml = `
+            <h4 class=>${data.last_update}</h4>
+            <h4>${infoCotizacion.source}</h4>
+            <h4>${infoCotizacion.value_sell}</h4>
+            <h4>${infoCotizacion.value_buy}</h4>
+                
+            `
+        }
+    });
 
-    const infoCotizacion = document.createElement("div");
-    infoCotizacion.innerHTML = `
-    <h4>${data.date}</h4>
-    <h4>${data.source}</h4>
-    <h4>${data.value_sell}</h4>
-    <h4>${data.value_buy}</h4>
-    `;
 
-    barraDeCotizacion.append(infoCotizacion);
-    } catch (error){
-        console.log(error);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let barraDeCotizacion =document.querySelector("#barraDeCotizacion");
+
+// const fetchCotizacion_barra = async () => {
+//     try {
+//     const response = await fetch("https://api.bluelytics.com.ar/v2/evolution.json");
+//     const data = await response.json();
+//     console.log(data);
+
+//     cotizacion.forEach((info) => {
+//         const infoCotizacion = document.createElement("div");
+//         infoCotizacion.innerHTML = `
+//             <h4>${info.date}</h4>
+//             <h4>${info.source}</h4>
+//             <h4>${info.value_sell}</h4>
+//             <h4>${info.value_buy}</h4>
     
-};
+//         `;  
+//     });
+    
 
-fetchCotizacion_barra();
+//     barraDeCotizacion.append(infoCotizacion);
+//     } catch (error){
+//         console.log(error);
+//     }
+    
+// };
+
+// fetchCotizacion_barra();
 
 
 
