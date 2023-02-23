c9dad7871cfc49a3b8ef598c0a3bde7be3689969
Edygar Franquiz <edyfranquiz@gmail.com>
2023-02-22 22:45:46 -0300

    se agreg{o api al archivo cotizacion_barra.js

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-JS Simulador Interactivo 
+Proyecto web Happy Journey
 🖖🏼Hola Mi nombre es Edygar Franquiz
 !Bienvenido a mi proyecto de CoderHouse de JavaScript! 😎🤍💻
+
+El proyecto consiste en una página web enfocada en la  planificación de viajes, Dónde se podrá encontrar una tienda, un blog de destinos, un simulador de divisas y una calculadora en dondé podrás calcular tu proximo destino. 
 
 
 Link de la página web ✈️ Happy Journey
--- js/cotizacion_barra.js
+++ js/cotizacion_barra.js
@@ -1,19 +1,21 @@
-let barraDeCotizacion =document.getElementById("barraDeCotizacion");
+let barraDeCotizacion = document.getElementById("barraDeCotizacion");
 
 fetch("https://api.bluelytics.com.ar/v2/evolution.json")
-    .then((resp) => resp.json())
+    .then((res) => res.json())
     .then((data) => {
-        console.log(data);
-        for (let infoCotizacion of data){
-            console.log(infoCotizacion);
+        // console.log(data)
+        for(let infoCotizacion of data){
+            console.log(infoCotizacion)
             let nuavaInfo = document.createElement("div")
             nuavaInfo.innerHtml = `
-            <h4 class=>${data.last_update}</h4>
+            <span class="barraDeCotizacion">💵</span>
+            <h4>${infoCotizacion.date}</h4>
             <h4>${infoCotizacion.source}</h4>
             <h4>${infoCotizacion.value_sell}</h4>
             <h4>${infoCotizacion.value_buy}</h4>
                 
-            `
+        `    
+        barraDeCotizacion.appendChild(nuavaInfo)
         }
     });
 
@@ -33,34 +35,6 @@ fetch("https://api.bluelytics.com.ar/v2/evolution.json")
 
 
 
-// let barraDeCotizacion =document.querySelector("#barraDeCotizacion");
-
-// const fetchCotizacion_barra = async () => {
-//     try {
-//     const response = await fetch("https://api.bluelytics.com.ar/v2/evolution.json");
-//     const data = await response.json();
-//     console.log(data);
-
-//     cotizacion.forEach((info) => {
-//         const infoCotizacion = document.createElement("div");
-//         infoCotizacion.innerHTML = `
-//             <h4>${info.date}</h4>
-//             <h4>${info.source}</h4>
-//             <h4>${info.value_sell}</h4>
-//             <h4>${info.value_buy}</h4>
-    
-//         `;  
-//     });
-    
-
-//     barraDeCotizacion.append(infoCotizacion);
-//     } catch (error){
-//         console.log(error);
-//     }
-    
-// };
-
-// fetchCotizacion_barra();
 
 
 
@@ -72,8 +46,16 @@ fetch("https://api.bluelytics.com.ar/v2/evolution.json")
 
 
 
-/* <h4>${data.rates.AED.rate_for_amount}</h4>
-Esa es la manera de acceder, de rate podés traer los otros 3 valores adicionales como AED (o el que elijas) AED.rate o AED.currency_name */
+
+
+
+
+
+
+
+
+
+
 
 
 
